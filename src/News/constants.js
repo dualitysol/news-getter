4f205bd3da988e20546c74058084d31e67e27c14
dualitysol <dualitysol@gmail.com>
2020-12-14 11:48:51 +0200

    hardcoded localurl removed

--- src/News/constants.js
+++ src/News/constants.js
@@ -1,2 +1,2 @@
-export const newsApiUrl = process.env.ENV === 'DEV' ? 'http://localhost:7777/uploads/response.json' : process.env.NEWS_API_URL || 'http://newsapi.org/v2/everything'
+export const newsApiUrl = process.env.NEWS_API_URL || 'http://newsapi.org/v2/everything'
 export const newsApiKey = process.env.NEWS_API_KEY || '56ebe961104d4ef4a60e4586250e11b7'
